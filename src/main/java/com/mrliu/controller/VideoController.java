4b81f60d2c43baff38cd5991aad08fe96d0a25fd
tfss <1255791430@qq.com>
2018-12-20 15:39:05 +0800

    ok

--- src/main/java/com/mrliu/controller/VideoController.java
+++ src/main/java/com/mrliu/controller/VideoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mrliu.common.utils.JsonUtil;
 import com.mrliu.generate.mapper.VideoMapper;
 import com.mrliu.generate.pojo.Video;
 import com.mrliu.generate.pojo.VideoExample;
@@ -26,15 +27,6 @@ public class VideoController {
         return "redirect:getVideoList";
     }
 	
-//	@RequestMapping("/getVideoList")  
-//    public String getVideoList(Model model){  
-//		
-//		VideoExample videoExample = new VideoExample();
-//		List<Video> videoList = DataAccessManager.getMapper(VideoMapper.class).selectByExample(videoExample);
-//		System.out.println(videoList);
-//		model.addAttribute("videoList", videoList);
-//		return "frontend/list";
-//	}
 	
 	@RequestMapping("/getVideoList")  
     public String getVideoList(Model model){  
@@ -49,10 +41,10 @@ public class VideoController {
 	@RequestMapping("/ckplayer")  
 	 public String viedo(Model model, Video video) {
 
-
-        model.addAttribute("videoInfo", video.getPath());
-        model.addAttribute("videoname", video.getVersion());
-        System.out.println("video:"+video.getPath());
+		VideoExample videoExample = new VideoExample();
+		videoExample.createCriteria().andIdEqualTo(video.getId());
+		Video video2 = DataAccessManager.getMapper(VideoMapper.class).selectByExample(videoExample).get(0);
+        model.addAttribute("videoInfo", video2);
         return "frontend/ckplayer";
     }
 }

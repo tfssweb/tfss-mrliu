f7dd7c4ad8b4a1a11b3c287d21c1caeadd216ecc
tfss <1255791430@qq.com>
2018-12-20 17:46:03 +0800

    sql

--- src/main/resources/doc/sql/db_mrliu.sql
+++ src/main/resources/doc/sql/db_mrliu.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50519
-Source Host           : localhost:3306
+Source Server         : 172.18.18.203
+Source Server Version : 50723
+Source Host           : 172.18.18.203:23306
 Source Database       : db_mrliu
 
 Target Server Type    : MYSQL
-Target Server Version : 50519
+Target Server Version : 50723
 File Encoding         : 65001
 
-Date: 2018-03-14 21:47:38
+Date: 2018-12-20 16:10:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,3 +29,29 @@ CREATE TABLE `t_admin` (
 -- ----------------------------
 -- Records of t_admin
 -- ----------------------------
+INSERT INTO `t_admin` VALUES ('1', 'admin', 'admin');
+
+-- ----------------------------
+-- Table structure for t_ckplayer_video
+-- ----------------------------
+DROP TABLE IF EXISTS `t_ckplayer_video`;
+CREATE TABLE `t_ckplayer_video` (
+  `id` int(11) NOT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `version` varchar(255) DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `introduce` varchar(255) DEFAULT NULL,
+  `thumbnail` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_ckplayer_video
+-- ----------------------------
+INSERT INTO `t_ckplayer_video` VALUES ('1', 'http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4', 'video/mp4', '中文标清', '0', '我的梦', '音乐MV', '就是一段音乐啊', null);
+INSERT INTO `t_ckplayer_video` VALUES ('2', 'http://vjs.zencdn.net/v/oceans.mp4', 'video/mp4', '中文高清', '0', '动物世界', '视频', '就是小动物们', null);
+INSERT INTO `t_ckplayer_video` VALUES ('3', 'http://img.ksbbs.com/asset/Mon_1703/eb048d7839442d0.mp4', 'video/mp4', '英文高清', '10', '我的梦', '音乐MV', '就是一段音乐啊', null);
+INSERT INTO `t_ckplayer_video` VALUES ('4', 'http://img.ksbbs.com/asset/Mon_1703/d30e02a5626c066.mp4', 'video/mp4', '英文超清', '0', '我的梦', '音乐MV', '就是一段音乐啊', null);

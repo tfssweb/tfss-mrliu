cb5032b2e092cb2fb954e51cfe8e5c9499324467
tfss <1255791430@qq.com>
2018-12-20 11:15:32 +0800

    v1.1版本

---
+++ src/main/java/com/mrliu/generate/mapper/VideoMapper.java
@@ -0,0 +1,30 @@
+package com.mrliu.generate.mapper;
+
+import com.mrliu.generate.pojo.Video;
+import com.mrliu.generate.pojo.VideoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface VideoMapper {
+    int countByExample(VideoExample example);
+
+    int deleteByExample(VideoExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Video record);
+
+    int insertSelective(Video record);
+
+    List<Video> selectByExample(VideoExample example);
+
+    Video selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Video record, @Param("example") VideoExample example);
+
+    int updateByExample(@Param("record") Video record, @Param("example") VideoExample example);
+
+    int updateByPrimaryKeySelective(Video record);
+
+    int updateByPrimaryKey(Video record);
+}
---
+++ src/main/java/com/mrliu/generate/pojo/Video.java
@@ -0,0 +1,68 @@
+package com.mrliu.generate.pojo;
+
+public class Video {
+    private Integer id;
+
+    private String path;
+
+    private String type;
+
+    private String version;
+
+    private Integer weight;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version == null ? null : version.trim();
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", path=").append(path);
+        sb.append(", type=").append(type);
+        sb.append(", version=").append(version);
+        sb.append(", weight=").append(weight);
+        sb.append("]");
+        return sb.toString();
+    }
+}

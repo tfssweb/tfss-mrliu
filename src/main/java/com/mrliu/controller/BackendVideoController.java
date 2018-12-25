a52d32cea21cdb520a454086cc5d79900523e652
tfss <1255791430@qq.com>
2018-12-25 08:46:09 +0800

    add back end controller

---
+++ src/main/java/com/mrliu/controller/BackendVideoController.java
@@ -0,0 +1,35 @@
+package com.mrliu.controller;
+
+import java.util.List;
+
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mrliu.generate.mapper.VideoMapper;
+import com.mrliu.generate.pojo.Video;
+import com.mrliu.generate.pojo.VideoExample;
+import com.mrliu.support.dao.DataAccessManager;
+
+@Controller 
+@RequestMapping(value = "/admin")
+public class BackendVideoController {
+
+	@Autowired
+	private Sid sid;
+	
+	
+//	获取所有视频列表
+	@RequestMapping("/getVideoList")  
+    public String getVideoList(Model model){  
+		
+		VideoExample videoExample = new VideoExample();
+		List<Video> videoList = DataAccessManager.getMapper(VideoMapper.class).selectByExample(videoExample);
+		System.out.println(videoList);
+		model.addAttribute("videoList", videoList);
+		return "frontend/videos";
+	}
+	
+}
